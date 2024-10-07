49d5e84326c1a8fe84b0aeeb33a063d4c5b29e33
ethanreidel <ethanjreidel@gmail.com>
2024-10-06 20:58:52 -0700

    pixel fixes + eraser

--- LD56_Tiny_Creatures/draw.gd
+++ LD56_Tiny_Creatures/draw.gd
@@ -1,20 +1,20 @@
 extends Node2D
 
-@onready var color_picker := $ColorPickerButton
-
 var in_box = false
 var area_rect = Rect2(0, 0, 64, 64)
 var color = "#E6EEEF"
 var grid = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
 	#prevent clearing of viewport for drawing purposes
 	RenderingServer.viewport_set_clear_mode(get_viewport().get_viewport_rid(), RenderingServer.VIEWPORT_CLEAR_NEVER)	
 	
 	#set up buttons and their connection to respective function
 	$Button.pressed.connect(self._on_button_pressed1)
 	$save_button.pressed.connect(self._on_button_pressed)
+	$pixel_size_button.pressed.connect(self._toggle_pixel_size_button_1x1)
+	$pixel_size_button2.pressed.connect(self._toggle_pixel_size_button_2x2)
+	$clear_button.pressed.connect(self._clear_image)
 
 #function to swap between color and eraser
 var c = 0
@@ -30,16 +30,33 @@ func _on_button_pressed():
 	print('Saving image...')
 	var capture = get_viewport().get_texture()
 	var image = capture.get_image()
-	var final_image = image.get_region(Rect2i(0, 0, 960, 1000))
+	var final_image = image.get_region(Rect2i(0, 0, 64, 64))
 	final_image.save_png("res://sample_image.png")
 	var culled: Image = Image.load_from_file("res://sample_image.png")
 	var sprite = make_image_transparent_and_spritize("res://sample_image.png")
 	print("done...")
 	
+var size = Vector2(1, 1)
+var arr = [Vector2(1, 1), Vector2(2, 2)]
+func _toggle_pixel_size_button_1x1():
+	size = arr[0]
+func _toggle_pixel_size_button_2x2():
+	size = arr[1]
+
+var clear = false
+func _clear_image():
+	clear = not clear
+	queue_redraw()
+	
 func _draw() -> void:
+	if clear == true:
+		var rect = Rect2(Vector2.ZERO, Vector2(64, 64))
+		draw_rect(rect, Color.BLACK)
+		clear = false
+		
 	#get screen position, and if left click -> call draw_circle at current position
 	var curr = get_local_mouse_position()
-	var rect_with_position = Rect2(Vector2(curr[0], curr[1]),  Vector2(1, 1))
+	var rect_with_position = Rect2(Vector2(curr[0], curr[1]), size)
 	draw_rect(rect_with_position, color)
 	
 func make_image_transparent_and_spritize(path):
